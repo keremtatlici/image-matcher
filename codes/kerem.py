b752f15e34dfe800604280b542b08a775c019074
keremtatlici <krmttlc@gmail.com>
2021-12-07 23:30:58 +0300

    Algorihm selected: SIFT and flann new beautiful results added ALMOST DONE :)

--- codes/kerem.py
+++ codes/kerem.py
@@ -1,13 +1,17 @@
 import cv2
 
-def resize_image(image, divided_by):
+def downsize_image(image, divided_by):
     
     return cv2.resize(image, (int(image.shape[1] / divided_by), int(image.shape[0] / divided_by)))
+
+def upsize_image(image, extended_by):
+    
+    return cv2.resize(image, (int(image.shape[1] * extended_by), int(image.shape[0] * extended_by)))
 
 def show_images(images, divided_by=None):
     for idx in range(len(images)):
         if divided_by is not None:
-            cv2.imshow(str(idx), resize_image(images[idx], divided_by))
+            cv2.imshow(str(idx), downsize_image(images[idx], divided_by))
         else:
             cv2.imshow(str(idx), images[idx])
     cv2.waitKey()
